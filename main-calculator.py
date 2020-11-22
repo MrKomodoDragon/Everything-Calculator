919605c1f2ff3bebef9a87260e357b490fb89068
Saravanavasan <vasan@Ramadasss-MacBook-Air.local>
2020-11-22 12:35:27 -0800

    Updated README

--- main-calculator.py
+++ main-calculator.py
@@ -9,6 +9,8 @@ root= tk.Tk()
 
 canvas1 = tk.Canvas(root, width = 400, height = 300)
 canvas1.pack()
+w = tk.Label(root, text="Hello Tkinter!")
+canvas1.create_window(200, 100, window=w)
 entry1 = tk.Entry (root)
 canvas1.create_window(200, 140, window=entry1)
 
@@ -16,6 +18,9 @@ def getCalc():
     x1 = entry1.get()
     if x1 == "1":
         graph()
+    elif x1 == "2":
+        basicTingz()
+
 button1 = tk.Button(text='Execute', command=getCalc)
 canvas1.create_window(200, 180, window=button1)
 
