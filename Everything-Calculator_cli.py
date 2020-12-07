2705b621b27fee97207255cf5916d69c42054a33
Saravanavasan <vasan@Ramadasss-MacBook-Air.local>
2020-12-07 11:34:37 -0800

    changed name

--- Everything-Calculator_cli.py
+++ Everything-Calculator_cli.py
@@ -164,4 +164,8 @@ def basicTingz():
         equation = input("Input your equation here (Make sure to use this sign \"/\" for multiplication!): ")
         solution = eval(equation)
         print(solution)
-
+calcType = input("What operation would you like to do? Type 1 for graphs, or 2 for basic operations such as addition and subtraction.")
+if calcType == "1":
+    graph()
+elif calcType == "2":
+    basicTingz()
